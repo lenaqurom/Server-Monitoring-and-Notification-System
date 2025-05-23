d66f72dd08b1b196077d343578d729938e00c654
Lina Qurom <you@example.com>
2025-05-23 04:00:39 +0300

    Persist the deserialized data to a MongoDB instance

---
+++ MessageProcessingAndAnomalyDetectionService/Interfaces/IMongoDbService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageProcessingAndAnomalyDetectionService.Models;
+
+namespace MessageProcessingAndAnomalyDetectionService.Interfaces
+{
+    public interface IMongoDbService
+    {
+        Task InsertStatisticsAsync(ServerStatistics statistics);
+    }
+}
---
+++ MessageProcessingAndAnomalyDetectionService/Services/MongoDbService.cs
@@ -0,0 +1,28 @@
+﻿using MessageProcessingAndAnomalyDetectionService.Interfaces;
+using MessageProcessingAndAnomalyDetectionService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MessageProcessingAndAnomalyDetectionService.Services
+{
+    public class MongoDbService : IMongoDbService
+    {
+        private readonly IMongoCollection<ServerStatistics> _collection;
+
+        public MongoDbService(string connectionString, string databaseName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            _collection = database.GetCollection<ServerStatistics>(collectionName);
+        }
+
+        public async Task InsertStatisticsAsync(ServerStatistics statistics)
+        {
+            await _collection.InsertOneAsync(statistics);
+        }
+    }
+}
